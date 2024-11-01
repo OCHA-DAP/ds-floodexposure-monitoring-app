a9bf1ff148de41131fe323f43a9a552218cd8f3b
hannahker <hannah.ker96@gmail.com>
2024-11-01 16:04:27 -0400

    Update database in script

--- populate_database.py
+++ populate_database.py
@@ -1,11 +1,18 @@
+import os
 from pathlib import Path
 
 import geopandas as gpd
 import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 from constants import ISO3S
 from pipelines import blob_utils, codab_utils, floodscan_utils
 
+load_dotenv()
+
+AZURE_DB_PW_DEV = os.getenv("AZURE_DB_PW_DEV")
+AZURE_DB_UID = os.getenv("AZURE_DB_UID")
 LOCAL_DATA_DIR = Path("pipelines/data")
 
 
@@ -22,16 +29,22 @@ def migrate_data_to_repo(clobber: bool = False):
     adm.to_file(LOCAL_DATA_DIR / "adm.shp")
 
 
-def load_data():
+def load_data(engine):
     print("Loading data...")
     migrate_data_to_repo()
-    data_out = {}
     adm = gpd.read_file(LOCAL_DATA_DIR / "adm.shp")
     for adm_level in range(3):
         adm[f"ADM{adm_level}_NAME"] = adm[f"ADM{adm_level}_EN"].fillna(
             adm[f"ADM{adm_level}_FR"]
         )
-    data_out["adm"] = adm
+    adm.drop(columns=["geometry"], inplace=True)
+    adm.to_sql(
+        "adm",
+        schema="app",
+        con=engine,
+        if_exists="replace",
+        index=False,
+    )
 
     def calculate_rolling(group, window=7):
         group[f"roll{window}"] = group["total_exposed"].rolling(window=window).mean()
@@ -39,6 +52,7 @@ def load_data():
 
     window = 7
     for iso3 in ISO3S:
+        print(f"Getting data for {iso3}")
         blob_name = floodscan_utils.get_blob_name(iso3, "exposure_tabular")
         df = blob_utils.load_parquet_from_blob(blob_name)
         df = df.merge(adm[["ADM1_PCODE", "ADM2_PCODE"]])
@@ -50,11 +64,19 @@ def load_data():
         )
         df["dayofyear"] = df["date"].dt.dayofyear
         df["eff_date"] = pd.to_datetime(df["dayofyear"], format="%j")
-        data_out[iso3] = df
-    print("data loaded...")
-    return data_out
+        df.to_sql(
+            "flood_exposure",
+            schema="app",
+            con=engine,
+            if_exists="append",
+            index=False,
+        )
 
 
 if __name__ == "__main__":
-    print("hello world!")
-    data = load_data()
+    print("Populating database...")
+    engine = create_engine(
+        f"postgresql+psycopg2://{AZURE_DB_UID}:{AZURE_DB_PW_DEV}@chd-rasterstats-dev.postgres.database.azure.com/postgres"
+    )
+    data = load_data(engine)
+    print("Database update completed.")
