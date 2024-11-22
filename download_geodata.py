5c189679e7717ecdf7b3383290a58681bc1111d1
hannahker <hannah.ker96@gmail.com>
2024-11-22 12:50:56 -0800

    Keep all geodata processing in this repo

--- download_geodata.py
+++ download_geodata.py
@@ -1,72 +1,46 @@
-import json
-import os
-from typing import Literal
+import geopandas as gpd
+import pandas as pd
 
-from azure.storage.blob import ContainerClient
-from dotenv import load_dotenv
-
-load_dotenv()
-
-PROD_BLOB_SAS = os.getenv("PROD_BLOB_SAS")
-DEV_BLOB_SAS = os.getenv("DEV_BLOB_SAS")
-CONTAINER = "projects"
+from constants import ADMS, ISO3S
+from utils import codab_utils
 
 
-def download_geojson_from_azure(
-    container_client,
-    blob_name,
-    output_path,
-):
-    """
-    Download a GeoJSON file from Azure Blob Storage using a SAS token.
-
-    Parameters:
-    -----------
-    container_client : azure.storage.blob.ContainerClient
-        Azure ContainerClient
-    blob_name : str
-        Name of the blob to download
-    output_path : str, optional
-        Path where to save the GeoJSON file.
-    """
-    print(f"Getting data for {blob_name}")
-    blob_client = container_client.get_blob_client(blob_name)
-    blob_data = blob_client.download_blob()
-    geojson_str = blob_data.readall().decode("utf-8")
-    geojson_data = json.loads(geojson_str)
-    if output_path:
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        with open(output_path, "w") as f:
-            json.dump(geojson_data, f)
-        print(f"GeoJSON saved to: {output_path}")
-
-
-def get_container_client(
-    container_name: str = "projects", stage: Literal["prod", "dev"] = "dev"
-):
-    sas = DEV_BLOB_SAS if stage == "dev" else PROD_BLOB_SAS
-    container_url = (
-        f"https://imb0chd0{stage}.blob.core.windows.net/"  # noqa
-        f"{container_name}?{sas}"  # noqa
+def clean_gdf(gdf):
+    gdf["name"] = gdf.apply(
+        lambda row: (
+            row[f"ADM{adm}_FR"]
+            if pd.notna(row.get(f"ADM{adm}_FR")) and row.get(f"ADM{adm}_FR") != ""
+            else row.get(f"ADM{adm}_EN", "")
+        ),
+        axis=1,
     )
-    return ContainerClient.from_container_url(container_url)
+    gdf.rename(columns={f"ADM{adm}_PCODE": "pcode"}, inplace=True)
+    gdf = gdf[["pcode", "name", "geometry"]]
+    return gdf
 
 
 if __name__ == "__main__":
-    container_client = get_container_client(CONTAINER)
-    blob_name = "my_spatial_data.geojson"
-    blobs = container_client.list_blobs(
-        name_starts_with="ds-floodexposure-monitoring/processed/geojson"
-    )
-
-    for blob in blobs:
-        blob_name = blob.name
-        if blob_name.endswith(".json"):
-            output_name = blob_name.split("/")[-1]
-            print(output_name)
-
-            gdf = download_geojson_from_azure(
-                container_client=container_client,
-                blob_name=blob_name,
-                output_path=f"assets/geo/{output_name}",
+    for adm in ADMS:
+        print(f"Processing geo data for admin {adm}...")
+        gdfs = []
+        for iso3 in ISO3S:
+            gdf = codab_utils.load_codab_from_blob(iso3, admin_level=adm)
+            if (iso3 in ["nga", "tcd"]) and (adm == 0):
+                gdf = gdf.dissolve()
+            gdfs.append(gdf)
+        gdf_all = pd.concat(gdfs)
+        if adm == 0:
+            gdf_all_outline = gdf_all.copy()
+            gdf_all_outline.geometry = gdf_all_outline.geometry.boundary
+            gdf_all_outline = gpd.GeoDataFrame(gdf_all_outline, geometry="geometry")
+            gdf_all_outline = clean_gdf(gdf_all_outline)
+            gdf_all_outline.to_file(
+                f"assets/geo/adm{adm}_outline.json", driver="GeoJSON"
             )
+
+        gdf_all.geometry = gdf_all.geometry
+        gdf_all = gpd.GeoDataFrame(gdf_all, geometry="geometry")
+        gdf_all = clean_gdf(gdf_all)
+
+        gdf_all.to_file(f"assets/geo/adm{adm}.json", driver="GeoJSON")
+    print("All data processed.")
